a51cfdfc8cbeef7657af20a0c79f7a386d3793e7
Yandhile <yandhilemaluleke@gmail.com>
2026-03-21 15:04:37 +0200

    initial scaffolding

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import DashboardView from '../views/DashboardView.vue'
+import ExpensesView from '../views/ExpensesView.vue'
+import SettingsView from '../views/SettingsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: '/', component: DashboardView },
+    { path: '/expenses', component: ExpensesView },
+    { path: '/settings', component: SettingsView },
+  ],
 })
 
 export default router
---
+++ src/views/DashboardView.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <v-container>
+    <h2 class="text-h5 mb-4">Dashboard</h2>
+    <p class="text-medium-emphasis">Investment overview coming soon.</p>
+  </v-container>
+</template>
