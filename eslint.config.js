190401239e9ff3547db5615fc003d802e6dca8ca
Yandhile <yandhilemaluleke@gmail.com>
2026-03-21 16:15:23 +0200

    fix(eslint.config.js): add Node.js globals override for netlify/functions directory

--- eslint.config.js
+++ eslint.config.js
@@ -21,6 +21,15 @@ export default defineConfig([
     },
   },
 
+  {
+    files: ['netlify/functions/**'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
 
